b9a68f90509c9978c191ad21b320cfceca5879ad
Sacha Oltmans <foodebeer@users.noreply.github.com>
2017-04-27 11:35:14 +0100

    Removed swarmtoken and managertoken
    And replaced them by  $(docker-machine ssh ucp0 docker swarm join-token -q worker|manager)

--- setupddc.sh
+++ setupddc.sh
@@ -10,17 +10,12 @@ fi
 echo "******************** Creating UCP cluster"
 docker-machine create -d virtualbox --virtualbox-memory "4096" ucp0 && docker-machine ssh ucp0 docker swarm init --advertise-addr $(docker-machine ip ucp0)
 echo "Swarm manager created"
-echo "Get Swarm manager token"
-docker-machine ssh ucp0 docker swarm join-token -q manager > managertoken
-echo "Get Swarm worker token"
-docker-machine ssh ucp0 docker swarm join-token -q worker > swarmtoken
-echo "--------- Swarm token: $(cat swarmtoken) and Manager token: $(cat managertoken)"
 
 echo "Creating manager nodes"
 for (( COUNT=1; COUNT \< $1; COUNT++))
 do
     echo "Creating UCP node $COUNT"
-    docker-machine create -d virtualbox --virtualbox-memory "4096" ucp$COUNT && docker-machine ssh ucp$COUNT docker swarm join --token $(cat managertoken) $(docker-machine ip ucp0)
+    docker-machine create -d virtualbox --virtualbox-memory "4096" ucp$COUNT && docker-machine ssh ucp$COUNT docker swarm join --token $(docker-machine ssh ucp0 docker swarm join-token -q manager) $(docker-machine ip ucp0)
 done
 echo "----------- Installing UCP ------------------"
 docker-machine ssh ucp0 docker run --rm --tty --name ucp -p 8080:80 -p 4443:443 -P -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --host-address $(docker-machine ip ucp0) --admin-username 'moby' --admin-password 'd!ck1234' --swarm-port 2378
@@ -40,7 +35,7 @@ rm swarmnodes
 for (( COUNT=0; COUNT < $3; COUNT++))
 do
 	echo "Creating node$COUNT"
-	docker-machine create -d virtualbox --engine-label environment=TESTLABEL node$COUNT  && echo "Created node$COUNT" && echo node$COUNT >> swarmnodes && docker-machine ssh node$COUNT docker swarm join --token $(cat swarmtoken) $(docker-machine ip ucp0)
+	docker-machine create -d virtualbox --engine-label environment=TESTLABEL node$COUNT  && echo "Created node$COUNT" && echo node$COUNT >> swarmnodes && docker-machine ssh node$COUNT docker swarm join --token $(docker-machine ssh ucp0 docker swarm join-token -q worker) $(docker-machine ip ucp0)
 done
 
 echo "***************** Create DTR cluster"
@@ -48,7 +43,7 @@ echo "----------- Creating first DTR node ----------"
 docker-machine create -d virtualbox --virtualbox-memory "3072" dtr0
 # Join Swarm
 echo "----------- DTR joining Swarm ---------"
-docker-machine ssh dtr0 docker swarm join --token $(cat swarmtoken) $(docker-machine ip ucp0)
+docker-machine ssh dtr0 docker swarm join --token $(docker-machine ssh ucp0 docker swarm join-token -q worker) $(docker-machine ip ucp0)
 echo "--------------- Installing DTR -----------"
 # Install DTR
 #docker-machine ssh dtr0 docker run --rm --tty --name dtr -p 8081:80 -p 4444:443 -P -v /var/run/docker.sock:/var/run/docker.sock docker/dtr install --ucp-url https://$(docker-machine ip ucp0) --dtr-external-url https://$(docker-machine ip dtr0)  --ucp-node dtr0 --ucp-username 'moby' --ucp-password 'd!ck1234' --ucp-insecure-tls --replica-id 1234567890AB
@@ -58,7 +53,7 @@ echo "--------------- Installing DTR nodes -------------"
 for (( COUNT=1; COUNT \< $2; COUNT++))
 do
     echo "Create DTR node $COUNT"
-    docker-machine create -d virtualbox --virtualbox-memory "3072" dtr$COUNT && docker-machine ssh dtr$COUNT docker swarm join --token $(cat swarmtoken) $(docker-machine ip ucp0)
+    docker-machine create -d virtualbox --virtualbox-memory "3072" dtr$COUNT && docker-machine ssh dtr$COUNT docker swarm join --token $(docker-machine ssh ucp0 docker swarm join-token -q worker) $(docker-machine ip ucp0)
     docker-machine ssh dtr$COUNT docker run --rm --tty -p 809$COUNT:80 844$COUNT:443 docker/dtr join --ucp-node dtr$COUNT --ucp-insecure-tls --ucp-url https://$(docker-machine ip ucp0) --ucp-username 'moby' --ucp-password 'd!ck1234' --existing-replica-id 1234567890AB --replica-http-port "809$COUNT" --replica-https-port "844$COUNT" --replica-id AB000000000$COUNT
 done
 
